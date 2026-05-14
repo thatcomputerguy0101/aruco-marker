7c6769001c22c09325700200fb718df5c84a28e2
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-05-13 21:03:51 -0400

    Use template literals for svg formatting

--- src/aruco-marker.ts
+++ src/aruco-marker.ts
@@ -41,26 +41,20 @@ export function arucoToSVGString(id: number, size?: number | string) {
 	let image = '';
 
 	if (size) {
-		size = 'height="' + size + '" width="' + size + '"';
+		size = `height="${size}" width="${size}"`;
 	} else {
 		size = '';
 	}
 
 	image =
-		'<svg ' +
-		size +
-		' viewBox="0 0 7 7" version="1.1" xmlns="http://www.w3.org/2000/svg">\n' +
+		`<svg ${size} viewBox="0 0 7 7" version="1.1" xmlns="http://www.w3.org/2000/svg">\n` +
 		'  <rect x="0" y="0" width="7" height="7" fill="black"/>\n';
 
 	for (y = 0; y < 5; y++) {
 		for (x = 0; x < 5; x++) {
 			if (marker[x][y] === 1) {
 				image +=
-					'  <rect x="' +
-					(x + 1) +
-					'" y="' +
-					(y + 1) +
-					'" width="1" height="1" fill="white" ' +
+					`  <rect x="${x + 1}" y="${y + 1}" width="1" height="1" fill="white" ` +
 					// Slight stroke to get around aliasing issues with adjacent rectangles
 					'stroke="white" stroke-width="0.01" />\n';
 			}
